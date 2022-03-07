e7ba587e4f69f3a722dd64d66904abe2d66f2900
arh <arh@monkeypeppershoe.com>
2022-03-07 13:42:16 -0800

    Successfully creating keys for target user

--- commands/newKey.js
+++ commands/newKey.js
@@ -2,14 +2,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios');
 // const { ApplicationCommandOptionTypes } = require('discord.js/typings/enums');
-const { airtable } = require('../config.json');
+const { oofauth } = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('newkey')
 		.setDescription('Lists commands')
-		.addUserOption(user =>
-			user.setName('user')
+		.addUserOption(option =>
+			option.setName('target')
 			.setDescription('The user the key is for')
 			.setRequired(true))
 		.addStringOption(duration =>
@@ -20,14 +20,15 @@ module.exports = {
 		//get all current api keys in list
 		//call api to create a record of the api key, along with user and duration
 		//return the user, key, and duration in message
+		const target = interaction.options.getUser('target');
 
 		var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     	var key = '';
     	for ( var i = 0; i < 20; i++ ) {
         	key += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
     	}
+    	key = String(target.tag).substring(0, String(target.tag).length-5).toUpperCase() + key;
 
-		const target = interaction.user.id;
 		const duration = interaction.options.getString("duration");
 		var suffix = String(duration).substring(String(duration).length-1);
 		var time = parseInt(String(duration).substring(0, String(duration).length-1));
@@ -49,7 +50,7 @@ module.exports = {
 		}
 
 		// let result = await fetch("http://164.92.65.15:5000/add_autogrinder", {headers:{"OofAuth": airtable}, "api_key": "PlaceHolder123", "discord_id": target}).then(resp => resp.json())		//create randomly generated key that is not already in database
-		let result = await axios.post("http://164.92.65.15:5000/add_autogrinder", {headers:{"OofAuth": airtable}, "api_key": key, "expire_after": time, "discord_id": target}) //.then(resp => resp.json())
-		await interaction.reply(`Target User: ${target}\nDuration: ${time}\nApiKey: ${key}`);
+		let result = await axios.post('http://164.92.65.15:5000/add_autogrinder', {api_key: key, expire_after: time, discord_id: target.id}, {headers:{"OofAuth": oofauth}}) //.then(resp => resp.json())
+		await interaction.reply(`Target User: ${target.username}\nDuration: ${time}\nApiKey: ${key}`);
 	},
 };
